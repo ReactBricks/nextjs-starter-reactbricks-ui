a38f79c2d27cf7e90a56de9cb98a297efbbf421f
Giorgio Tognoli <giorgio@f2.net>
2022-10-05 18:31:11 +0200

    Updated packages ( react-bricks v3.6.1 )

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -3,6 +3,7 @@ import website from 'react-bricks-ui/website'
 import blog from 'react-bricks-ui/blog'
 import HeroUnit from './MyHeroUnit'
 
+
 const bricks: types.Brick<any>[] = [
   ...website, // React Bricks UI
   ...blog,
